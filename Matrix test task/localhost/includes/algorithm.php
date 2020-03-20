a722d12584ed5170c7b3839fc36591652f8ed7b7
Prodiler <triplasm@yandex.ru>
2020-03-21 00:33:55 +0300

    Minor changes

--- Matrix test task/localhost/includes/algorithm.php
+++ Matrix test task/localhost/includes/algorithm.php
@@ -2,29 +2,23 @@
   function get_result($str) // Функция, получающая финальный результат
   {
     $arr_opt = array();
-    echo 'Данные из файла: <br>';
-    echo $str;
-    echo '<br>';
+    echo 'Данные из файла: <br>' . $str . '<br>';
     $arr_opt = all_opt_get($str, $arr_opt);
     return $arr_opt;
   }
 
   function save_first_new($str) // Сохраняет варианты строк с раскрытыми внешними фигурными скобками
   {
-    $count = 0; $opt = array(); $count_open = 0; $count_close = 0;
+    $count = 0; $opt = array();
     for ($i=0; $i < strlen($str); $i++) {
       if ($str[$i] == '{' && $count == 0) { // Первое вхождение скобочного выражения
-        $index = $i;
-        $count++;
+        $index = $i; $count++;
         $opt = brack_save($str, $i, $opt)[0];
         $i = brack_save($str, $i, $opt)[1];
-        $count_open = 0; $count_close = 0;
       }
-
       if ($str[$i] == '|') { // Переход к следующему скобочному выражению
         $opt = brack_save($str, $i, $opt)[0];
         $i = brack_save($str, $i, $opt)[1] - 1;
-        $count_open = 0; $count_close = 0;
       }
       if ($str[$i] == '}' && $str[$i+1] != '}' && $str[$i+1] != '|' && count($opt) > 0) { // Конец считывания
         for ($k=0; $k < count($opt); $k++) { // Присоединение начала строки
@@ -42,6 +36,7 @@
   function brack_save($str, $i, $opt) // Сохранение скобочного выражения вида текст{...}текст
   {
     $temp_str = '';  $j = $i + 1;
+    $count_open = 0; $count_close = 0;
     while(!($count_open == $count_close && ($str[$j] == '|' || $str[$j] == '}'))) {
       if ($str[$j] == '{')
         $count_open++;
@@ -52,6 +47,7 @@
     }
     array_push($opt,$temp_str);
     $res_arr = array($opt, $j);
+    $count_open = 0; $count_close = 0;
     return $res_arr;
   }
 
@@ -65,7 +61,7 @@
     $deep = array(); // Глубина вложенности (число фигурных скобок) для всех частей
     for ($i=0; $i < count($temp_arr); $i++) {
       array_push($deep,substr_count($temp_arr[$i],'{')); // Глубина
-      while ($deep[$i] != 0) { // Пройдет мимо
+      while ($deep[$i] != 0) { // Рекурсивно вызываем функцию, пока не получим все варианты раскрытия строки
         $final_res = all_opt_get($temp_arr[$i],$final_res);
         $deep[$i]--;
       }
--- Matrix test task/localhost/includes/aux_functions.php
+++ Matrix test task/localhost/includes/aux_functions.php
@@ -3,25 +3,26 @@
   function join_arr($arr, $types=0) // Объединение элементов массива по заданному правилу
   {
     $res_fields = '';
-    if ($types == 1) {
+    if ($types == 0) { // Объединение элементов массива в строку для выполнения запроса на вставку новых строк
+      $parse_type = '\', \'';
+      for ($i=0; $i < count($arr); $i++) {
+        $res_fields =  $res_fields . $arr[$i] . $parse_type;
+      }
+      $res_fields = substr($res_fields, 0, -4);
+    }
+    if ($types == 1) { // Объединение элементов массива в строку для выполнения запроса на добавление новых столбцов
       $parse_type = " TEXT NOT NULL DEFAULT 'Не задан', ";
       for ($i=0; $i < count($arr); $i++) {
         $res_fields =  $res_fields . $arr[$i] . $parse_type;
       }
       $res_fields = substr($res_fields, 0, -2);
     }
-    if ($types == 2) {
+    if ($types == 2) { // Объединение элементов массива в строку для выполнения запроса на создание индекса UNIQUE
       $parse_type = '(25), ';
       for ($i=0; $i < count($arr); $i++) {
         $res_fields =  $res_fields . $arr[$i] . $parse_type;
       }
       $res_fields = substr($res_fields, 0, -2);
-    }
-    if ($types == 0) {
-      for ($i=0; $i < count($arr); $i++) {
-        $res_fields =  $res_fields . $arr[$i] . '\', \'';
-      }
-      $res_fields = substr($res_fields, 0, -4);
     }
     return $res_fields;
   }
@@ -29,11 +30,11 @@
   function refresh($mysqli) // Удаление и создание новой таблицы
   {
     if ($mysqli->query("DROP TABLE `Results`") === TRUE) {
-      echo "Таблица удалена\r\n";
+      echo "<br>Таблица удалена<br>";
     }
     $sql_cr = "CREATE TABLE `Results` (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY)";
     if ($mysqli->query($sql_cr) === TRUE) {
-      echo("Таблица успешно создана\n");
+      echo "Таблица успешно создана<br>";
     }
   }
 ?>
